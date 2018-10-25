37ebe1111018bfb440a1125a478270cdd68a16e4
Vincent Mahnke <info@vimaster.de>
2018-10-25 13:10:09 +0200

    Public port is now changed with install script

--- install.js
+++ install.js
@@ -2,17 +2,24 @@
 const path = require('path');
 const fs = require('fs');
 
-const pathToConfig = path.join(__dirname, "config.yaml");
-
-fs.readFile(pathToConfig, 'utf8', function(err, data) {
+fs.readFile(path.join(__dirname, "config.yaml"), 'utf8', function(err, data) {
 	if (err) {
 		return console.log(err);
 	}
 
 	var result = data.replace(/serverPort: .*/, `serverPort: ${process.env.npm_package_config_parsoidPort}`);
-	fs.writeFileSync(pathToConfig, result, 'utf8');
+	fs.writeFileSync(path.join(__dirname, "config.yaml"), result, 'utf8');
 
-	fs.createReadStream(pathToConfig).pipe(fs.createWriteStream(path.join(__dirname, "node_modules", "parsoid", "config.yaml")));
+	fs.createReadStream(path.join(__dirname, "config.yaml")).pipe(fs.createWriteStream(path.join(__dirname, "node_modules", "parsoid", "config.yaml")));
+});
+
+fs.readFile(path.join(__dirname, "index.js"), 'utf8', function(err, data) {
+	if (err) {
+		return console.log(err);
+	}
+
+	var result = data.replace("    base: 'http://localhost:3000'", `    base: 'http://localhost:${process.env.npm_package_config_parsoidPort}'`);
+	fs.writeFileSync(path.join(__dirname, "index.js"), result, 'utf8');
 });
 
 
